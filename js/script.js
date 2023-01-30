f40f402f85cfef0291031a422f4823bd5109926e
gyeogyeo <rudns531@gmail.com>
2023-01-30 21:21:41 +0900

    sing-first_0130

--- js/script.js
+++ js/script.js
@@ -8,9 +8,29 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    type: "fraction",
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
+
+const tabItem = document.querySelectorAll('.sing_tab')
+const tabInner = document.querySelectorAll('.tab_inner')
+
+tabItem.forEach((tab, idx) => {
+  tab.addEventListener('click', function (e) {
+    e.preventDefault();
+    tabInner.forEach((inner) => {
+      inner.classList.remove('active')
+    })
+    
+    tabItem.forEach((item) => {
+      item.classList.remove('active')
+    })
+    
+    tabItem[idx].classList.add('active');
+    tabInner[idx].classList.add('active');
+  })
+})
